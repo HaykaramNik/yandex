ffeb052583f2a565f9b96d1d24050cedc3baa4b3
HaykaramNik <nhaykaram@gmail.com>
2023-04-25 05:33:44 +0400

    добавил sp1-3-1

--- sp1-3-1.cpp
+++ sp1-3-1.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
+		vector<pair<int, string>> names;
     int count;
+		cout << "enter count" << endl;
     cin >> count;
     for (int i = 0; i < count; ++i) {
-        string name;
+				string name;
         int age;
-        cin >> name >> age;
+				cout << "enter name" << endl;
+        cin >> name;
+				cout << "enter age" << endl;
+				cin >> age;
         // сохраните в вектор пар
+				names.push_back({age, name});
     }
+
+		sort(names.begin(), names.end());
+		reverse(names.begin(), names.end());
+
+		for (const auto& element : names)
+		{
+				cout << element.second << endl;
+		}
 
     // выведите только имена в порядке убывания возраста
     // Jack
