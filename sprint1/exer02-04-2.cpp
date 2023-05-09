1e810a191c5d701de0786e1c417e5be2a9a58993
HaykaramNik <nhaykaram@gmail.com>
2023-05-10 02:56:57 +0400

    02-05-1

--- sprint1/exer02-04-2.cpp
+++ sprint1/exer02-04-2.cpp
@@ -1,6 +1,28 @@
+#include <iostream>
 #include <numeric>
+#include <string>
+#include <set>
+
+using namespace std;
+
+string addWord(const string& accumulated, const string& word)
+{
+    return accumulated + " -" + word;
+}
 
 string AddStopWords(const string& query, const set<string>& stop_words) {
     // ...
-    
+    string tmp = "";
+    string str = accumulate(stop_words.begin(), stop_words.end(), tmp,  addWord);
+    return query + str;
+}
+
+int main ()
+{
+    set<string> stop_words{"of", "in"};
+    string query = "some tasty oranges";
+
+    cout << AddStopWords(query, stop_words) << endl;
+
+    return 0;
 }
